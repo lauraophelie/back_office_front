53d54d5eb5b931aab85d857270b4666dc3b13e2a
DiamondraAndriam <118435008+DiamondraAndriam@users.noreply.github.com>
2024-01-29 15:50:53 +0300

    update & test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //import {  BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.scss'
+// './App.scss'
 import AjoutModel from './pages/AjoutModele';
 
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,17 @@
 import Button from './Button'
 
+import '../assets/scss/app.scss'
+
 function NavBar(){
     return(
         <div className="navbar">
-            <Button  text='Statistique' icon ='' className = ''/>
-            <Button  text='Marque' icon ='' className = ''/>
-            <Button  text='Modèle' icon ='' className = ''/>
-            <Button  text='Type de vitesse' icon ='' className = ''/>
-            <Button  text="Type d'energie" icon ='' className = ''/>
-            <Button  text='Annonces' icon ='' className = ''/>
-            <Button  text='Se déconnecter' icon ='' className = ''/>
+            <Button  text='Statistique' icon ='' className = 'navbar_button'/>
+            <Button  text='Marque' icon ='' className = 'navbar_button'/>
+            <Button  text='Modèle' icon ='' className = 'navbar_button'/>
+            <Button  text='Type de vitesse' icon ='' className = 'navbar_button'/>
+            <Button  text="Type d'energie" icon ='' className = 'navbar_button'/>
+            <Button  text='Annonces' icon ='' className = 'navbar_button'/>
+            <Button  text='Se déconnecter' icon ='' className = 'navbar__button__logout'/>
         </div>
     )
 }
