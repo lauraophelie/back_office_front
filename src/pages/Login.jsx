0fe77a1e6406a8cb2dd5b8a86abd7a0004166a59
DiamondraAndriam <118435008+DiamondraAndriam@users.noreply.github.com>
2024-01-30 03:25:05 +0300

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import LoginButton from '../components/LoginButton';
 import InputComponent from '../components/InputComponent'
 import LoginPicture from '../components/LoginPicture';
@@ -5,6 +7,23 @@ import LoginPicture from '../components/LoginPicture';
 import '../assets/scss/login.scss'
 
 function Login() {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+       };
+       
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+       };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Ici, vous pouvez envoyer les données du formulaire à votre serveur
+        console.log(`Password: ${password}, Email: ${email}`);
+       };
+
     return (
         <div className='login'>
             <LoginPicture
@@ -15,23 +34,29 @@ function Login() {
             <div className='login__box'>
                 <div className='login__content'>
                     <h1>Log In</h1>
+                    <form onSubmit={handleSubmit}>
                     <InputComponent
                         label ="Adresse e-mail"
                         name = "email"
+                        onChange={handleEmailChange}
                         required = {true}
                         type = "text"
+                        value = "admin"
                         placeholder = "Ecrivez ici"
                     />
                     <InputComponent
                         label ="Mot de passe"
-                        name = "email"
+                        name = "password"
+                        onChange={handlePasswordChange}
                         required = {true}
                         type = "text"
+                        value = "0"
                         placeholder = "Ecrivez ici"
                     />
                     <LoginButton 
                         text = "Se connecter"
                     />
+                    </form>
                 </div>
             </div>
         </div>
