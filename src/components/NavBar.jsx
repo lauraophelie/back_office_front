d567657d0830a6d98db28106ae9d42f5aedbd128
DiamondraAndriam <118435008+DiamondraAndriam@users.noreply.github.com>
2024-01-31 07:51:08 +0300

    Revert "CSS"
    This reverts commit 8975391745945b49f667594124eda3ca5a717727.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,14 +7,12 @@ import '../assets/scss/app.scss'
 function NavBar(){
     return(
         <div className="navbar">
-            <div className="navlink">
-                <Link to="/statistique">  <Button  text='Statistiques' icon ='' className = 'navbar_button'/> </Link>
-                <Link to="/listeMarque">  <Button  text='Marque' icon ='' className = 'navbar_button'/> </Link>
-                <Link to="/listeModele">  <Button  text='Modèle' icon ='' className = 'navbar_button'/> </Link>
-                <Link to="/typeVitesse">  <Button  text='Type de vitesse' icon ='' className = 'navbar_button'/> </Link>
-                <Link to="/typeEnergie">  <Button  text="Type d'energie" icon ='' className = 'navbar_button'/> </Link>
-                <Link to="/listeAnnonces">  <Button  text='Annonces' icon ='' className = 'navbar_button'/> </Link>
-            </div>
+            <Link to="/statistique">  <Button  text='Statistique' icon ='' className = 'navbar_button'/> </Link>
+            <Link to="/listeMarque">  <Button  text='Marque' icon ='' className = 'navbar_button'/> </Link>
+            <Link to="/listeModele">  <Button  text='Modèle' icon ='' className = 'navbar_button'/> </Link>
+            <Link to="/typeVitesse">  <Button  text='Type de vitesse' icon ='' className = 'navbar_button'/> </Link>
+            <Link to="/typeEnergie">  <Button  text="Type d'energie" icon ='' className = 'navbar_button'/> </Link>
+            <Link to="/listeAnnonces">  <Button  text='Annonces' icon ='' className = 'navbar_button'/> </Link>
             <Link to="/logout">  <Button  text='Se déconnecter' icon ='' className = 'navbar_button_logout'/> </Link>
         </div>
     )
