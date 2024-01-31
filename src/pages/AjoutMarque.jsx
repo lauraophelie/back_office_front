5e6f16fd58cd95dc775113763710157970fe4e62
DiamondraAndriam <118435008+DiamondraAndriam@users.noreply.github.com>
2024-01-31 07:51:00 +0300

    Revert "Ajout Marque"
    This reverts commit c925d81c4fe61185dc2931b5f816a2bfcb8acc55.

--- src/pages/AjoutMarque.jsx
+++ src/pages/AjoutMarque.jsx
@@ -1,30 +1,8 @@
-import NavBar from '../components/NavBar'
-
 function AjoutMarque(){
     return(
-        <body>
-            <div className="app__container">
-                <NavBar/>
-                <div className="add__marque__container">
-                    <div className="profil__container">
-                        <img src="" alt="profil" />
-                        <div className="detail">
-                            <p className="nameprofil">Nom profil</p>
-                            <p className="emailprofil">Email</p>
-                        </div>
-                    </div>
-                    <div className="add__box">
-                        <h2>Ajouter une marque</h2>
-                        <form action="" className='form__add'>
-                            <label htmlFor="add_marque" className="label" id="labeladd">Nom de la marque</label>
-                            <input type="text" name="add_marque" id="add_marque" className="input__text" placeholder='Ecrire ici'/>
-
-                        </form>
-                        <button type="submit" className='add__btn'>Ajouter</button>
-                    </div>
-                </div>
-            </div>
-        </body>
+        <>
+            AjoutMarque
+        </>
     )
 }
 
